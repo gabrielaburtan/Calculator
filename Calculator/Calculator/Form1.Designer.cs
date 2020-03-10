8fadf5991dab3ced4b8548f067d5ac3369360e7a
gabrielaburtan <burtangabriela@gmail.com>
2020-03-10 16:35:44 +0200

    Added project files.

---
+++ Calculator/Calculator/Form1.Designer.cs
@@ -0,0 +1,382 @@
+﻿namespace Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CE = new System.Windows.Forms.Button();
+            this.C = new System.Windows.Forms.Button();
+            this.radical = new System.Windows.Forms.Button();
+            this.plusminus = new System.Windows.Forms.Button();
+            this._7 = new System.Windows.Forms.Button();
+            this._8 = new System.Windows.Forms.Button();
+            this._9 = new System.Windows.Forms.Button();
+            this.impartire = new System.Windows.Forms.Button();
+            this.procent = new System.Windows.Forms.Button();
+            this._4 = new System.Windows.Forms.Button();
+            this._5 = new System.Windows.Forms.Button();
+            this._6 = new System.Windows.Forms.Button();
+            this.inmultire = new System.Windows.Forms.Button();
+            this.invers = new System.Windows.Forms.Button();
+            this._1 = new System.Windows.Forms.Button();
+            this._2 = new System.Windows.Forms.Button();
+            this._3 = new System.Windows.Forms.Button();
+            this.scadere = new System.Windows.Forms.Button();
+            this._0 = new System.Windows.Forms.Button();
+            this.virgula = new System.Windows.Forms.Button();
+            this.adunare = new System.Windows.Forms.Button();
+            this.egal = new System.Windows.Forms.Button();
+            this.rezultat = new System.Windows.Forms.TextBox();
+            this.ecuatie = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // CE
+            // 
+            this.CE.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CE.Location = new System.Drawing.Point(40, 71);
+            this.CE.Name = "CE";
+            this.CE.Size = new System.Drawing.Size(71, 45);
+            this.CE.TabIndex = 0;
+            this.CE.Text = "CE";
+            this.CE.UseVisualStyleBackColor = true;
+            this.CE.Click += new System.EventHandler(this.CE_Click);
+            // 
+            // C
+            // 
+            this.C.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.C.Location = new System.Drawing.Point(119, 71);
+            this.C.Name = "C";
+            this.C.Size = new System.Drawing.Size(68, 45);
+            this.C.TabIndex = 1;
+            this.C.Text = "C";
+            this.C.UseVisualStyleBackColor = true;
+            this.C.Click += new System.EventHandler(this.C_Click);
+            // 
+            // radical
+            // 
+            this.radical.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radical.Location = new System.Drawing.Point(244, 71);
+            this.radical.Name = "radical";
+            this.radical.Size = new System.Drawing.Size(45, 45);
+            this.radical.TabIndex = 2;
+            this.radical.Text = "sqrt";
+            this.radical.UseVisualStyleBackColor = true;
+            this.radical.Click += new System.EventHandler(this.radical_Click);
+            // 
+            // plusminus
+            // 
+            this.plusminus.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.plusminus.Location = new System.Drawing.Point(193, 71);
+            this.plusminus.Name = "plusminus";
+            this.plusminus.Size = new System.Drawing.Size(45, 45);
+            this.plusminus.TabIndex = 3;
+            this.plusminus.Text = "+/-";
+            this.plusminus.UseVisualStyleBackColor = true;
+            this.plusminus.Click += new System.EventHandler(this.plusminus_Click);
+            // 
+            // _7
+            // 
+            this._7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this._7.Location = new System.Drawing.Point(40, 122);
+            this._7.Name = "_7";
+            this._7.Size = new System.Drawing.Size(45, 45);
+            this._7.TabIndex = 4;
+            this._7.Text = "7";
+            this._7.UseVisualStyleBackColor = true;
+            this._7.Click += new System.EventHandler(this.button_Click);
+            // 
+            // _8
+            // 
+            this._8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this._8.Location = new System.Drawing.Point(91, 122);
+            this._8.Name = "_8";
+            this._8.Size = new System.Drawing.Size(45, 45);
+            this._8.TabIndex = 5;
+            this._8.Text = "8";
+            this._8.UseVisualStyleBackColor = true;
+            this._8.Click += new System.EventHandler(this.button_Click);
+            // 
+            // _9
+            // 
+            this._9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this._9.Location = new System.Drawing.Point(142, 122);
+            this._9.Name = "_9";
+            this._9.Size = new System.Drawing.Size(45, 45);
+            this._9.TabIndex = 6;
+            this._9.Text = "9";
+            this._9.UseVisualStyleBackColor = true;
+            this._9.Click += new System.EventHandler(this.button_Click);
+            // 
+            // impartire
+            // 
+            this.impartire.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.impartire.Location = new System.Drawing.Point(193, 122);
+            this.impartire.Name = "impartire";
+            this.impartire.Size = new System.Drawing.Size(45, 45);
+            this.impartire.TabIndex = 7;
+            this.impartire.Text = "/";
+            this.impartire.UseVisualStyleBackColor = true;
+            this.impartire.Click += new System.EventHandler(this.operatii_Click);
+            // 
+            // procent
+            // 
+            this.procent.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.procent.Location = new System.Drawing.Point(244, 122);
+            this.procent.Name = "procent";
+            this.procent.Size = new System.Drawing.Size(45, 45);
+            this.procent.TabIndex = 8;
+            this.procent.Text = "%";
+            this.procent.UseVisualStyleBackColor = true;
+            this.procent.Click += new System.EventHandler(this.operatii_Click);
+            // 
+            // _4
+            // 
+            this._4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this._4.Location = new System.Drawing.Point(40, 173);
+            this._4.Name = "_4";
+            this._4.Size = new System.Drawing.Size(45, 45);
+            this._4.TabIndex = 9;
+            this._4.Text = "4";
+            this._4.UseVisualStyleBackColor = true;
+            this._4.Click += new System.EventHandler(this.button_Click);
+            // 
+            // _5
+            // 
+            this._5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this._5.Location = new System.Drawing.Point(91, 173);
+            this._5.Name = "_5";
+            this._5.Size = new System.Drawing.Size(45, 45);
+            this._5.TabIndex = 10;
+            this._5.Text = "5";
+            this._5.UseVisualStyleBackColor = true;
+            this._5.Click += new System.EventHandler(this.button_Click);
+            // 
+            // _6
+            // 
+            this._6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this._6.Location = new System.Drawing.Point(142, 173);
+            this._6.Name = "_6";
+            this._6.Size = new System.Drawing.Size(45, 45);
+            this._6.TabIndex = 11;
+            this._6.Text = "6";
+            this._6.UseVisualStyleBackColor = true;
+            this._6.Click += new System.EventHandler(this.button_Click);
+            // 
+            // inmultire
+            // 
+            this.inmultire.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inmultire.Location = new System.Drawing.Point(193, 173);
+            this.inmultire.Name = "inmultire";
+            this.inmultire.Size = new System.Drawing.Size(45, 45);
+            this.inmultire.TabIndex = 12;
+            this.inmultire.Text = "*";
+            this.inmultire.UseVisualStyleBackColor = true;
+            this.inmultire.Click += new System.EventHandler(this.operatii_Click);
+            // 
+            // invers
+            // 
+            this.invers.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.invers.Location = new System.Drawing.Point(244, 173);
+            this.invers.Name = "invers";
+            this.invers.Size = new System.Drawing.Size(45, 45);
+            this.invers.TabIndex = 13;
+            this.invers.Text = "1/x";
+            this.invers.UseVisualStyleBackColor = true;
+            this.invers.Click += new System.EventHandler(this.invers_Click);
+            // 
+            // _1
+            // 
+            this._1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this._1.Location = new System.Drawing.Point(40, 224);
+            this._1.Name = "_1";
+            this._1.Size = new System.Drawing.Size(45, 45);
+            this._1.TabIndex = 14;
+            this._1.Text = "1";
+            this._1.UseVisualStyleBackColor = true;
+            this._1.Click += new System.EventHandler(this.button_Click);
+            // 
+            // _2
+            // 
+            this._2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this._2.Location = new System.Drawing.Point(91, 224);
+            this._2.Name = "_2";
+            this._2.Size = new System.Drawing.Size(45, 45);
+            this._2.TabIndex = 15;
+            this._2.Text = "2";
+            this._2.UseVisualStyleBackColor = true;
+            this._2.Click += new System.EventHandler(this.button_Click);
+            // 
+            // _3
+            // 
+            this._3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this._3.Location = new System.Drawing.Point(142, 224);
+            this._3.Name = "_3";
+            this._3.Size = new System.Drawing.Size(45, 45);
+            this._3.TabIndex = 16;
+            this._3.Text = "3";
+            this._3.UseVisualStyleBackColor = true;
+            this._3.Click += new System.EventHandler(this.button_Click);
+            // 
+            // scadere
+            // 
+            this.scadere.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scadere.Location = new System.Drawing.Point(193, 224);
+            this.scadere.Name = "scadere";
+            this.scadere.Size = new System.Drawing.Size(45, 45);
+            this.scadere.TabIndex = 17;
+            this.scadere.Text = "-";
+            this.scadere.UseVisualStyleBackColor = true;
+            this.scadere.Click += new System.EventHandler(this.operatii_Click);
+            // 
+            // _0
+            // 
+            this._0.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this._0.Location = new System.Drawing.Point(40, 275);
+            this._0.Name = "_0";
+            this._0.Size = new System.Drawing.Size(96, 45);
+            this._0.TabIndex = 18;
+            this._0.Text = "0";
+            this._0.UseVisualStyleBackColor = true;
+            this._0.Click += new System.EventHandler(this.button_Click);
+            // 
+            // virgula
+            // 
+            this.virgula.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.virgula.Location = new System.Drawing.Point(142, 275);
+            this.virgula.Name = "virgula";
+            this.virgula.Size = new System.Drawing.Size(45, 45);
+            this.virgula.TabIndex = 19;
+            this.virgula.Text = ".";
+            this.virgula.UseVisualStyleBackColor = true;
+            this.virgula.Click += new System.EventHandler(this.virgula_Click);
+            // 
+            // adunare
+            // 
+            this.adunare.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.adunare.Location = new System.Drawing.Point(193, 275);
+            this.adunare.Name = "adunare";
+            this.adunare.Size = new System.Drawing.Size(45, 45);
+            this.adunare.TabIndex = 20;
+            this.adunare.Text = "+";
+            this.adunare.UseVisualStyleBackColor = true;
+            this.adunare.Click += new System.EventHandler(this.operatii_Click);
+            // 
+            // egal
+            // 
+            this.egal.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.egal.Location = new System.Drawing.Point(244, 224);
+            this.egal.Name = "egal";
+            this.egal.Size = new System.Drawing.Size(45, 96);
+            this.egal.TabIndex = 21;
+            this.egal.Text = "=";
+            this.egal.UseVisualStyleBackColor = true;
+            this.egal.Click += new System.EventHandler(this.egal_Click);
+            // 
+            // rezultat
+            // 
+            this.rezultat.Location = new System.Drawing.Point(40, 13);
+            this.rezultat.Multiline = true;
+            this.rezultat.Name = "rezultat";
+            this.rezultat.Size = new System.Drawing.Size(249, 36);
+            this.rezultat.TabIndex = 22;
+            this.rezultat.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // ecuatie
+            // 
+            this.ecuatie.AutoSize = true;
+            this.ecuatie.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ecuatie.Location = new System.Drawing.Point(41, 14);
+            this.ecuatie.Name = "ecuatie";
+            this.ecuatie.Size = new System.Drawing.Size(0, 16);
+            this.ecuatie.TabIndex = 23;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(332, 328);
+            this.Controls.Add(this.ecuatie);
+            this.Controls.Add(this.rezultat);
+            this.Controls.Add(this.egal);
+            this.Controls.Add(this.adunare);
+            this.Controls.Add(this.virgula);
+            this.Controls.Add(this._0);
+            this.Controls.Add(this.scadere);
+            this.Controls.Add(this._3);
+            this.Controls.Add(this._2);
+            this.Controls.Add(this._1);
+            this.Controls.Add(this.invers);
+            this.Controls.Add(this.inmultire);
+            this.Controls.Add(this._6);
+            this.Controls.Add(this._5);
+            this.Controls.Add(this._4);
+            this.Controls.Add(this.procent);
+            this.Controls.Add(this.impartire);
+            this.Controls.Add(this._9);
+            this.Controls.Add(this._8);
+            this.Controls.Add(this._7);
+            this.Controls.Add(this.plusminus);
+            this.Controls.Add(this.radical);
+            this.Controls.Add(this.C);
+            this.Controls.Add(this.CE);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CE;
+        private System.Windows.Forms.Button C;
+        private System.Windows.Forms.Button radical;
+        private System.Windows.Forms.Button plusminus;
+        private System.Windows.Forms.Button _7;
+        private System.Windows.Forms.Button _8;
+        private System.Windows.Forms.Button _9;
+        private System.Windows.Forms.Button impartire;
+        private System.Windows.Forms.Button procent;
+        private System.Windows.Forms.Button _4;
+        private System.Windows.Forms.Button _5;
+        private System.Windows.Forms.Button _6;
+        private System.Windows.Forms.Button inmultire;
+        private System.Windows.Forms.Button invers;
+        private System.Windows.Forms.Button _1;
+        private System.Windows.Forms.Button _2;
+        private System.Windows.Forms.Button _3;
+        private System.Windows.Forms.Button scadere;
+        private System.Windows.Forms.Button _0;
+        private System.Windows.Forms.Button virgula;
+        private System.Windows.Forms.Button adunare;
+        private System.Windows.Forms.Button egal;
+        private System.Windows.Forms.TextBox rezultat;
+        private System.Windows.Forms.Label ecuatie;
+    }
+}
+
---
+++ Calculator/Calculator/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        Double value=0;
+        String operatie = "";
+        bool ok = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void button_Click(object sender, EventArgs e)
+        {
+            if((rezultat.Text=="0"||(ok)))
+                rezultat.Clear();
+            ok = false;
+            Button B = new Button();
+            B = sender as Button;
+            rezultat.Text = rezultat.Text + B.Text;
+        }
+        private void operatii_Click(object sender, EventArgs e)
+        {
+            Button B = (Button)sender;
+            operatie = B.Text;
+            value = Double.Parse(rezultat.Text);
+            ok = true;
+            ecuatie.Text = value + " " + operatie;
+        }
+        private void CE_Click(object sender, EventArgs e)
+        {
+            rezultat.Text = "0";
+        }
+
+        private void egal_Click(object sender, EventArgs e)
+        {
+            ecuatie.Text = "";
+            switch (operatie)
+            {
+                case "+":
+                    rezultat.Text = (value + Double.Parse(rezultat.Text)).ToString();
+                    break;
+                case "-":
+                    rezultat.Text = (value - Double.Parse(rezultat.Text)).ToString();
+                    break;
+                case "*":
+                    rezultat.Text = (value * Double.Parse(rezultat.Text)).ToString();
+                    break;
+                case "/":
+                    rezultat.Text = (value / Double.Parse(rezultat.Text)).ToString();
+                    break;
+                case "%":
+                    rezultat.Text = (value % Double.Parse(rezultat.Text)).ToString();
+                    break;
+                default:
+                    break;
+            }
+            
+        }
+        private void C_Click(object sender, EventArgs e)
+        {
+            rezultat.Text = "0";
+            value = 0;
+        }
+        private void sqrt_click(object sender, EventArgs e)
+        {
+            double x = Convert.ToDouble(rezultat.Text);
+            double y = Math.Sqrt(x);
+            rezultat.Text = x.ToString();
+        }
+
+        private void virgula_Click(object sender, EventArgs e)
+        {
+            rezultat.Text = rezultat.Text + ".";
+        }
+
+        private void plusminus_Click(object sender, EventArgs e)
+        {
+            double x = Convert.ToDouble(rezultat.Text);
+	        double y = -x;
+            rezultat.Text = y.ToString();
+        }
+
+        private void radical_Click(object sender, EventArgs e)
+        {
+            double x = Convert.ToDouble(rezultat.Text);
+            double y = Math.Sqrt(x);
+            rezultat.Text = y.ToString();
+        }
+
+        private void invers_Click(object sender, EventArgs e)
+        {
+            double x = Convert.ToDouble(rezultat.Text);
+            double y = 1 / x;
+            rezultat.Text = y.ToString();
+        }
+    }
+
+}
